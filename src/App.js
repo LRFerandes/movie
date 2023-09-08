8cdbe7d60e30fbd9290aa9b59b71ea7c62f53f3c
Lauro Nunes <104803481+LRFerandes@users.noreply.github.com>
2023-09-08 15:43:12 -0300

    Front-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from "./img/sala-cinema.jpg";
 
 function App() {
+
+  const estiloDoFundo = {
+    backgroundImage: `url(${Logo})`, 
+    backgroundSize: 'cover',                 
+    backgroundRepeat: 'no-repeat',
+             
+
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main' style={estiloDoFundo}>
+      <div className='boxForm'>
+        <form className='form'>
+          
+          <input type='text' id='name' placeholder='Filme'></input>
+          
+          <input type='text' id='genero' placeholder='genero'></input>
+          
+          <input type='text' id='horario' placeholder='Horario'></input>
+         
+          <input type='text' id='data' placeholder='data'></input>
+        
+          <input type='text' id='local' placeholder='local'></input>
+          
+        </form>
+        <button className='button'>
+            Create
+          </button>
+      </div>
+ 
     </div>
   );
 }
