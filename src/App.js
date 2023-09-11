af56c59923916a1d73c1c6ce735fdc8de98794f8
Lauro Nunes <104803481+LRFerandes@users.noreply.github.com>
2023-09-11 09:11:27 -0300

    Front-end
    tela de create

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
+import React from 'react';
 import './App.css';
 import Logo from "./img/sala-cinema.jpg";
 
-function App() {
 
-  const estiloDoFundo = {
-    backgroundImage: `url(${Logo})`, 
-    backgroundSize: 'cover',                 
-    backgroundRepeat: 'no-repeat',
-             
+export default class app extends React.Component{
 
-  };
-  
-  return (
-    <div className='main' style={estiloDoFundo}>
-      <div className='boxForm'>
-        <form className='form'>
-          
-          <input type='text' id='name' placeholder='Filme'></input>
-          
-          <input type='text' id='genero' placeholder='genero'></input>
-          
-          <input type='text' id='horario' placeholder='Horario'></input>
-         
-          <input type='text' id='data' placeholder='data'></input>
-        
-          <input type='text' id='local' placeholder='local'></input>
-          
-        </form>
-        <button className='button'>
-            Create
-          </button>
-      </div>
- 
-    </div>
-  );
+      state = {
+
+        filme:"",
+        genero:"",
+        horario:"",
+        data:"",
+        local: ""
+      }
+
+      print = () => {
+        alert("Filme: " + this.state.filme + "\n genero: " + this.state.genero + "\n horario: " + this.state.horario + "\n data: " + this.state.data + "\n local: " + this.state.local)
+      }
+
+    render() {
+
+      
+
+      const estiloDoFundo = {
+        backgroundImage: `url(${Logo})`, 
+        backgroundSize: 'cover',                 
+        backgroundRepeat: 'no-repeat',
+                
+
+      };
+      
+      return (
+
+        <div className='main' style={estiloDoFundo}>
+          <div className='boxForm'>
+            <form className='form'>
+              
+              <input type='text' id='name' placeholder='Filme' onChange={(e) => {this.setState({filme: e.target.value})}}></input>
+              
+              <input type='text'  id='genero' placeholder='Genero' onChange={(e) => {this.setState({genero: e.target.value})}}></input>
+              
+              <input type='time'  id='horario' placeholder='Horario' onChange={(e) => {this.setState({horario: e.target.value})}}></input>
+            
+              <input type='date' id='data' placeholder='Data' onChange={(e) => {this.setState({data: e.target.value})}}></input>
+            
+              <input type='text' id='local' placeholder='Local' onChange={(e) => {this.setState({local: e.target.value})}}></input>
+              
+            </form>
+            <button className='button' onClick={this.print} >
+                Create
+              </button>
+          </div>
+    
+        </div>
+      );
+    }
 }
-
-export default App;
